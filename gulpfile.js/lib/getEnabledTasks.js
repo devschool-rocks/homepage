730bdc74fa3f391759baed5f74caa441510347c5
Jim OKelly <thatrubylove@gmail.com>
2016-03-31 11:54:21 -0600

    added sitemap to default tasks

--- gulpfile.js/lib/getEnabledTasks.js
+++ gulpfile.js/lib/getEnabledTasks.js
@@ -3,7 +3,7 @@ var compact = require('lodash/array/compact')
 
 // Grouped by what can run in parallel
 var assetTasks = ['fonts', 'iconFont', 'images', 'svgSprite']
-var codeTasks = ['html', 'css', 'js']
+var codeTasks = ['html', 'css', 'js', 'sitemap']
 
 module.exports = function(env) {
 
