73fbf1c9ac3fc0bfb9ad6eb55abc9ab421c03b25
Jim OKelly <thatrubylove@gmail.com>
2016-03-31 12:09:27 -0600

    added some seo content fixes

--- gulpfile.js/lib/getEnabledTasks.js
+++ gulpfile.js/lib/getEnabledTasks.js
@@ -3,7 +3,7 @@ var compact = require('lodash/array/compact')
 
 // Grouped by what can run in parallel
 var assetTasks = ['fonts', 'iconFont', 'images', 'svgSprite']
-var codeTasks = ['html', 'css', 'js', 'sitemap']
+var codeTasks = ['html', 'css', 'js']
 
 module.exports = function(env) {
 
