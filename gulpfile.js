89f97ec5139a2af291d0c317d37ed23448091382
Jim OKelly <jim@devschool.rocks>
2016-04-10 20:22:24 -0500

    done for the day, basically good

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-'use strict';
 var site        = require('./site.json');
 site.time       = new Date();
 
@@ -21,15 +20,17 @@ var gulp   = require('gulp'),
     bSync  = require('browser-sync'),
     reload = bSync.reload;
 
+
 var collectPosts = function() {
   var posts = [];
 
   return through.obj(function (file, enc, cb) {
     posts.push(file.page);
-    var post = posts[posts.length - 1];
-    post.body = file.contents.toString();
-    post.summary = summarize(file.contents.toString(), '<!--more-->');
-    post.permalink = file.path.split("blog/")[1];
+    var post          = posts[posts.length - 1];
+    post.body         = file.contents.toString();
+    post.summary      = summarize(file.contents.toString(), '<!--more-->');
+    post.permalink    = file.path.split("blog/")[1];
+    post.publishedOn  = /(\d{4}\/\d{2}\/\d{2})/.exec(file.path)[0];
     this.push(file);
     cb();
   },
@@ -54,68 +55,67 @@ var summarize = function(contents, marker) {
 // JS
 gulp.task('js', function() {
   return gulp.src(['src/js/**/*.js'])
-                               .pipe(sMaps.init())
-                               .pipe(concat("app.min.js"))
-                               .pipe(uglify())
-                               .pipe(sMaps.write())
-                               .pipe(gulp.dest('dist/js'))
-                               .pipe(reload({stream: true}));
-                               });
+             .pipe(sMaps.init())
+             .pipe(concat("app.min.js"))
+             .pipe(uglify())
+             .pipe(sMaps.write())
+             .pipe(gulp.dest('dist/js'))
+             .pipe(reload({stream: true}));
+});
 
 // IMAGES
 gulp.task('images', function() {
 gulp.src("src/images/**/*")
-      .pipe(imgMin())
-      .pipe(gulp.dest('dist/images'))
-      });
+    .pipe(imgMin())
+    .pipe(gulp.dest('dist/images'))
+});
 
 // SCSS
 gulp.task('css', function () {
-  return gulp.src(['src/sass/**/*.scss', 'src/sass/**/*.css'])
-                                                       .pipe(sMaps.init())
-                                                       .pipe(concat("app.min.css"))
-                                                       .pipe(sass({outputStyle: 'compressed', errLogToConsole: true}))
-                                                       .pipe(sMaps.write())
-                                                       .pipe(gulp.dest('dist/css'))
-                                                       .pipe(reload({stream: true}));
-                                                       });
+  return gulp.src(['src/sass/**/*.scss'])
+             .pipe(sMaps.init())
+             .pipe(concat("app.min.css"))
+             .pipe(sass({outputStyle: 'compressed', errLogToConsole: true}))
+             .pipe(sMaps.write())
+             .pipe(gulp.dest('dist/css'))
+             .pipe(reload({stream: true}));
+});
 
 
 // HTML
 gulp.task('html', function() {
-return gulp.src(['src/**/*.html'])
-                                           .pipe(data({site: site}))
-                                           .pipe(render({
-                                           path: ['src/templates']
-                                           }))
-                                           .pipe(minify({collapseWhitespace: true}))
-                                           .pipe(gulp.dest('dist'))
-                                           .pipe(reload({stream: true}));
-                                           });
+  return gulp.src(['src/**/*.html'])
+             .pipe(data({site: site}))
+             .pipe(render({
+               path: ['src/templates']
+             }))
+             .pipe(minify({collapseWhitespace: true}))
+             .pipe(gulp.dest('dist'))
+             .pipe(reload({stream: true}));
+});
 
 // Markdown
 gulp.task('blog', function() {
-return gulp.src(['src/blog/**/*.md'])
-                               .pipe(page({property: "page", remove: true}))
-                               .pipe(mDown())
-                               .pipe(collectPosts())
-                               .pipe(wrap(function (data) {
-                               return fs.readFileSync('src/templates/blog.html').toString()
-                               }, null, {engine: 'nunjucks'}))
-                               .pipe(gulp.dest('dist'))
-                               .pipe(reload({stream: true}));
-                               });
+  return gulp.src(['src/blog/**/*.md'])
+             .pipe(page({property: "page", remove: true}))
+             .pipe(mDown())
+             .pipe(collectPosts())
+             .pipe(wrap(function (data) {
+               return fs.readFileSync('src/templates/blog.html').toString()
+             }, null, {engine: 'nunjucks'}))
+             .pipe(gulp.dest('dist'))
+             .pipe(reload({stream: true}));
+});
 
 // BOWER
 gulp.task('bower', function() { 
-return bower()
-.pipe(gulp.dest('bower_components')) 
+  return bower().pipe(gulp.dest('bower_components')) 
 });
 
 gulp.task('fonts', function() { 
-return gulp.src(['bower_components/font-awesome/fonts/**.*', 'src/fonts/**/*']) 
-                                                                            .pipe(gulp.dest('./dist/fonts')); 
-                                                                            });
+  return gulp.src(['bower_components/font-awesome/fonts/**.*', 'src/fonts/**/*']) 
+             .pipe(gulp.dest('./dist/fonts')); 
+});
 
 // BROWSER SYNC
 gulp.task('sync', function() {
@@ -126,8 +126,8 @@ gulp.task('sync', function() {
 gulp.task('watch', function() {
   gulp.watch('src/js/**/*.js', ['js']);
   gulp.watch('src/sass/**/*.scss', ['css']);
-  gulp.watch(['src/**/*.html'], ['html']);
-  gulp.watch(['src/blog/**/*'], ['blog']);
+  gulp.watch(['src/**/*.html'], ['blog', 'html']);
+  gulp.watch(['src/blog/**/*'], ['blog', 'html']);
   gulp.watch('src/images/**/*', ['images']);
 });
 
